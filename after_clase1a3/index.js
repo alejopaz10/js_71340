308b675c29632c15cfdb1fa0710e3dd91fd4ba90
“Alejo <alejopaz35@gmail.com>
2024-05-22 18:20:10 -0300

    after

---
+++ after_clase1a3/index.js
@@ -0,0 +1,48 @@
+// CAJERO AUTOMATICO
+/** 
+ * 1 VER SALOD
+ * 2 INGRESAR DINERO
+ * 3 RETIRAR PARTE DEL DINERO
+ * 4 RETIRAR TODO EL DINERO
+ * 5 SALIR
+ */
+
+// let saldo = 0;
+
+// let option;
+
+// do{
+//     option=prompt(`
+//     Ingrese una de las siguiente opciones :
+//     * 1 VER SALDO
+//     * 2 INGRESAR DINERO
+//     * 3 RETIRAR PARTE DEL DINERO
+//     * 4 RETIRAR TODO EL DINERO
+//     * 0 SALIR
+//     `);
+//     switch (option) {
+//         case "1":
+//             alert(saldo)
+//             break;
+//         case"2":
+//             const moneyIn = Number(prompt("Cuanto dinero desea ingresar: "));
+//             saldo += moneyIn
+//             alert ("Dinero ingresado con exito , su nuevo saldo es : " + saldo )
+//             break;
+//         case"3":
+//         const moneyOutParcial = Number(prompt("Cuanto dinero desea retirar"));
+//         saldo -=moneyOutParcial
+//         alert("Dinero retirado con exito, su nuevo saldo es: " + saldo);
+//             break;
+//         case"4":
+//         saldo = 0
+//         alert("Dinero retirado con exito, su nuevo saldo es: " + saldo);
+//             break;
+//         case "0":
+//             alert("Gracias por su visita")
+//             break;
+//         default:
+//             alert("Ingrese una opcion valida");
+//             break;
+//     }
+// } while (option !=0)
